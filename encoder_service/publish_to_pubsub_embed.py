64875b3133e72e087d0db75437772a8f648bf290
Mark <mark.edmondson@cellpointmobile.com>
2023-05-22 14:28:28 +0200

    source

--- encoder_service/publish_to_pubsub_embed.py
+++ encoder_service/publish_to_pubsub_embed.py
@@ -202,7 +202,7 @@ def data_to_embed_pubsub(data: dict, vector_name:str="documents"):
 
         docs = []
         for url in urls:
-            metadata["url"] = url
+            metadata["source"] = url
             metadata["type"] = "url_load"
             doc = read_url_to_document(url, metadata=metadata)
             docs.extend(doc)
