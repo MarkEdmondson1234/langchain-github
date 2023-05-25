cc37d0767b60251cabfb6e3eedbe712356205506
Mark <mark.edmondson@cellpointmobile.com>
2023-05-25 19:43:38 +0200

    sent data

--- webapp/bot_help.py
+++ webapp/bot_help.py
@@ -26,7 +26,7 @@ def discord_webhook(message_data):
     logging.info(f'Sending discord this data: {data}')
     response = requests.post(webhook_url, json=data,
                             headers={'Content-Type': 'application/json'})
-    logging.info(f'Sent data to discord: {response.json()}')
+    logging.info(f'Sent data to discord: {response}')
     
     return response
 
