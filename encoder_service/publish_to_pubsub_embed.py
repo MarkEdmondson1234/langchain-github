121be5875a6fc31f985078cad27ea1a142c495f1
Mark <mark.edmondson@cellpointmobile.com>
2023-05-26 23:41:22 +0200

    attributes

--- encoder_service/publish_to_pubsub_embed.py
+++ encoder_service/publish_to_pubsub_embed.py
@@ -197,8 +197,8 @@ def data_to_embed_pubsub(data: dict, vector_name:str="documents"):
             # https://cloud.google.com/storage/docs/json_api/v1/objects#resource-representations
             message_data = 'gs://' + attributes.get("bucketId") + '/' + attributes.get("objectId")
 
-            if '/' in data["objectId"]:
-                bucket_vector_name = data["objectId"].split('/')[0]
+            if '/' in attributes.get("objectId"):
+                bucket_vector_name = attributes.get("objectId").split('/')[0]
 
                 if len(bucket_vector_name) > 0 and vector_name != bucket_vector_name:
                     logging.info(f"Overwriting vector_name {vector_name} with {bucket_vector_name}")
