1831afb81f03ac51e01b5ec2eda15f4ed63a24b0
Mark <mark.edmondson@cellpointmobile.com>
2023-06-03 11:09:11 +0200

    support vertex embeddings and qna #1

--- qna/question_service.py
+++ qna/question_service.py
@@ -1,12 +1,14 @@
-import os, logging
+import os, logging, sys
 
 from langchain.vectorstores import SupabaseVectorStore
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
 
+from langchain.llms import VertexAI
+from langchain.embeddings import VertexAIEmbeddings
+
 #https://python.langchain.com/en/latest/modules/chains/index_examples/chat_vector_db.html
 from langchain.chains import ConversationalRetrievalChain
-import openai.error as errors
 
 from supabase import Client, create_client
 from dotenv import load_dotenv
@@ -15,13 +17,27 @@ load_dotenv()
 
 def qna(question: str, vector_name: str, chat_history=None):
 
+    llm = None
+    embeddings = None
+    llm_str = 'openai' if sys.getenv('OPENAI_API_KEY', None) is not None else 'vertex'
+    logging.info(f'Using embeddings: {llm_str}')
+
+    if llm_str == 'openai':
+        llm = OpenAI(temperature=0)
+        embeddings = OpenAIEmbeddings()
+    elif llm_str == 'vertex':
+        llm = VertexAI(temperature=0)
+        embeddings = VertexAIEmbeddings()
+    else:
+        raise NotImplementedError(f'No llm implemented for {llm_str}')
+
     logging.info(f"Initiating Supabase store: {vector_name}")
     # init embedding and vector store
     supabase_url = os.getenv('SUPABASE_URL')
     supabase_key = os.getenv('SUPABASE_KEY')
 
     logging.info(f"Supabase URL: {supabase_url} vector_name: {vector_name}")
-    embeddings = OpenAIEmbeddings()
+    
     supabase: Client = create_client(supabase_url, supabase_key)
 
     vectorstore = SupabaseVectorStore(supabase, 
@@ -43,10 +59,5 @@ def qna(question: str, vector_name: str, chat_history=None):
                                                max_tokens_limit=3500)
 
     result = qa({"question": question, "chat_history": chat_history})
-    # try:
-    # except errors.InvalidRequestError as error:
-    #     result = {"answer": "The prompt given was too big", "error": str(error)}
-    # except Exception as error:
-    #     result = {"answer": f"An error occurred - {str(error)}", "error": str(error)}
     
     return result
